5608712296bfa49c1ab31f63b83a9d1f360790da
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-09-05 23:13:49 -0400

    ADD ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,20 @@
+//1. Declara una variable llamada nombre y asígnale un valor de tipo string.
+//2. Declara una variable llamada edad y asígnale un valor de tipo number.
+//3. Declara una variable llamada puedoConducir y asígnale un valor de tipo boolean.
+//4. Para cada variable se debe imprimir por consola el resultado de cada variable declarada.
+//5. Para cada variable se debe imprimir el tipo de dato que es por consola.
+
+//Declaración de variables
+const nombre = 'Anita';
+const edad = 21;
+const puedoConducir = true;
+
+//Imprime por consola el resultado 
+console.log(nombre)
+console.log(edad)
+console.log(puedoConducir)
+
+//Imprime el tipo de dato 
+console.log(typeof nombre); 
+console.log(typeof edad);
+console.log(typeof puedoConducir);
