244869acfddfc46944bf0baf7771710f94c0be42
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-09-06 22:44:22 -0400

    ADD ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,31 @@
+//1. Suma (+): Suma numero1 y numero2. Muestra el resultado por consola.
+//2. Resta (-): Resta numero1 y numero2. Muestra el resultado por consola.
+//3. Multiplicación (*): Multiplica numero1 y numero2. Muestra el resultado por consola.
+//4. División (/): Divide numero1 y numero2. Muestra el resultado por consola.
+//5. Modulo (%):  Saca el modulo entre numero2 y numero1. Muestra el resultado por consola.
+
+const numero1 = 20;
+const numero2 = 40; 
+
+
+//Suma
+const suma = numero1 + numero2;
+console.log("El resultado de la suma es: ", suma);
+
+//Resta
+const resta = numero1 - numero2;
+console.log("El resultado de la resta es: ", resta);
+
+//Multiplicación
+const mult = numero1 * numero2;
+console.log("El resultado de la multiplicación es: ", mult);
+
+//División
+const div = numero1 / numero2;
+console.log("El resultado de la división es: ", div);
+
+//Módulo
+const mod = numero2 % numero1;
+console.log("El resultado del módulo es: ", mod);
+
+
