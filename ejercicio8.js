ef420a60acb7c048d45b523184fdc097c4798286
Ana Aroca <ana.aroca2101@alumnos.ubiobio.cl>
2024-09-08 23:04:22 -0300

    ADD ejercicio8.js

---
+++ ejercicio8.js
@@ -0,0 +1,31 @@
+//1. **Crear un objeto de persona**: Define un objeto llamado "persona" que tenga propiedades como nombre, edad y género. Luego, 
+//muestra la información de esa persona por la consola.
+const persona = {
+    nombre: "Anita",
+    edad: 21,
+    género: "Femenino"
+}
+console.log(persona);
+
+
+//2. Crear un objeto llamado caja: La caja se compone de cuadernos, lápices, papel, fotografías y indica el estado si esta en buen estado o no. 
+//Imprimir el objeto entero por consola, imprimir cada propiedad del objeto y el tipo de dato que es.
+const caja = {
+    cuadernos: 2,
+    lapices: 5,
+    papel: 10,
+    fotografias: 60,
+    estado: "Buen estado"
+}
+console.log(caja);
+console.log(caja.cuadernos);
+console.log(caja.lapices);
+console.log(caja.papel);
+console.log(caja.fotografias);   
+console.log(caja.estado);
+console.log(typeof caja);
+console.log(typeof caja.cuadernos);
+console.log(typeof caja.lapices);
+console.log(typeof caja.papel);
+console.log(typeof caja.fotografias);
+console.log(typeof caja.estado);
